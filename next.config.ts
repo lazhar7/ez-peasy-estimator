2df835c7284688db8d8a0e46d958e98758e7a79f
lazhar7 <lazhar@moverlead.com>
2026-04-05 01:33:10 -0400

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-        reactStrictMode: true,
-        typescript: { ignoreBuildErrors: true },
-        eslint: { ignoreDuringBuilds: true },
-        turbopack: {},
-        images: {
-                  remotePatterns: [
-                        {
-                                      protocol: "https",
-                                      hostname: "photos.zillowstatic.com",
-                        },
-                            ],
-        },
+          reactStrictMode: true,
+          typescript: { ignoreBuildErrors: true },
+          eslint: { ignoreDuringBuilds: true },
+          images: {
+                      remotePatterns: [
+                              {
+                                              protocol: "https",
+                                              hostname: "photos.zillowstatic.com",
+                              },
+                                  ],
+          },
 };
 
 export default nextConfig;
