541b5579fa2ee36ea44011e66ac216cbda731a74
Lazhar <lazhar7@gmail.com>
2026-04-04 22:56:59 -0400

    fix: next config

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // App will be served under /crm via reverse proxy or basePath
-  // Uncomment below if deploying as standalone under /crm path
-  // basePath: '/crm',
   reactStrictMode: true,
   images: {
     remotePatterns: [
